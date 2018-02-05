5fd812bcf3bd2b4e7bd95b8a8b2bcfca7bf8af0a
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-02-05 16:11:13 -0500

    init

---
+++ routes/inventory.js
@@ -0,0 +1,14 @@
+const rp = require('request-promise-native')
+const uuidv1 = require('uuid/v1')
+
+async function inventoryRoutes(fastify, options) {
+  fastify.route({
+    method: '',
+    url: '',
+    handler: async (request, reply) => {
+
+    }
+  })
+}
+
+module.exports = inventoryRoutes
